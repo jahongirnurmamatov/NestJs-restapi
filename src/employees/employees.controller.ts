5c91ff42ea1ce976220a57b95ccb19d770541b57
jahongiir.nurmamatov <zokir.almatov1414@gmail.com>
2025-01-16 14:55:11 +0500

    Throttler and adding api prefix

--- src/employees/employees.controller.ts
+++ src/employees/employees.controller.ts
@@ -1,7 +1,9 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete,Query } from '@nestjs/common';
 import { EmployeesService } from './employees.service';
 import { Prisma } from '@prisma/client';
+import { Throttle,SkipThrottle } from '@nestjs/throttler';
 
+@SkipThrottle()
 @Controller('employees')
 export class EmployeesController {
   constructor(private readonly employeesService: EmployeesService) {}
@@ -10,12 +12,13 @@ export class EmployeesController {
   create(@Body() createEmployeeDto: Prisma.EmployeeCreateInput) {
     return this.employeesService.create(createEmployeeDto);
   }
-
+  @SkipThrottle({default:false})
   @Get()
   findAll(@Query('role') role?: 'Intern' | 'Engineer' | 'Admin') {
     return this.employeesService.findAll(role);
   }
 
+  @Throttle({short:{ttl:1000,limit:1}})
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.employeesService.findOne(+id);
